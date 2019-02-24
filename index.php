705929b325c6174c95fc22456ed6808ed9542668
Toni Codina <tcodinat@gmail.com>
2019-02-24 15:29:29 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,114 @@
+<html>
+<head>
+<!-- Primary Meta Tags -->
+<title>Alyssa - Designer, developer, and entrepreneur</title>
+<meta name="title" content="Alyssa - Designer, developer, and entrepreneur">
+<meta name="description" content="Alyssa is a designer & full-stack developer with an entrepreneurial mindset. She is the founder of several ventures such as Noon Studios and Twiverse">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://alyssax.com/">
+<meta property="og:title" content="Alyssa - Designer, developer, and entrepreneur">
+<meta property="og:description" content="Alyssa X is a designer & full-stack developer with an entrepreneurial mindset. She is the founder of several ventures such as Noon Studios and Twiverse">
+<meta property="og:image" content="https://alyssax.com/assets/meta.png">
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://alyssax.com/">
+<meta property="twitter:site" content="alyssaxuu">
+<meta property="twitter:title" content="Alyssa - Designer, developer, and entrepreneur">
+<meta property="twitter:description" content="Alyssa X is a designer & full-stack developer with an entrepreneurial mindset. She is the founder of several ventures such as Noon Studios and Twiverse">
+<meta property="twitter:image" content="https://alyssax.com/assets/meta.png">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:600" rel="stylesheet">
+<link rel="stylesheet" href="styles.css">
+<link rel="icon" href="favicon.ico">
+</head>
+<body>
+	<div id="head">
+		<p id="name">Alyssa X</p>
+		<div id="right">
+			<span id="links">
+				<a href="https://twitter.com/alyssaxuu" target="_blank">Twitter</a>
+				<a href="https://dribbble.com/alyssax" target="_blank">Dribbble</a>
+				<a href="https://github.com/alyssaxuu" target="_blank">GitHub</a>
+				<a href="https://medium.com/@alyssax" target="_blank">Medium</a>
+			</span>
+			<a id="hi" href="mailto:hi@alyssax.com" target="_blank">hi@alyssax.com <img src="assets/wave.png"></a>
+		</div>
+		<div id="line"><div id="highlight"></div></div>
+	</div>	
+	<div id="top">
+		<img id="heart" src="assets/cute.png">
+		<h1>Alyssa is a <span>designer</span>, <span>full-stack developer</span> and <span>entrepreneur</span>. She is the founder of <a href="https://studiosnoon.com" target="_blank">Noon Studios</a> &amp; several other ventures.</h1>
+		<h2>She has been recognized as the <span>Best Woman Maker of 2018</span> by Maker Mag, and nominated as <span>Maker of The Year</span> by Product Hunt. She was also featured on <span>YCombinator’s HackerNews</span>, <span>Diversity30</span>, <span>Product Hunt</span> and interviewed by <span>IndieHackers</span> for one of her successful products.</h2>
+		<div id="socialsmall">
+			<a href="https://twitter.com/alyssaxuu" target="_blank"><img src="assets/twitter.svg"></a>
+			<a href="https://dribbble.com/alyssax" target="_blank"><img src="assets/dribbble.svg"></a>
+			<a href="https://github.com/alyssaxuu" target="_blank"><img src="assets/github.svg"></a>
+			<a href="https://medium.com/@alyssax" target="_blank"><img src="assets/medium.svg"></a>
+		</div>
+	</div>
+	<div class="section">
+		<div class="intro">PRODUCTS <img src="assets/lipstick.png"></div>
+		<hr>
+		<div class="item">
+			<a href="https://twiverse.com" target="_blank">Twiverse</a>
+			<h3>Discover diverse Twitter users from different backgrounds</h3>
+			<img src="https://cdn.dribbble.com/users/886569/screenshots/5459050/ezgif.com-optimize__10_.gif">
+		</div>
+		<div class="item">
+			<a href="https://exposure.cards" target="_blank">Exposure Cards</a>
+			<h3>Get more traffic to your content &amp; find underrated websites</h3>
+			<img src="https://cdn.dribbble.com/users/886569/screenshots/5459050/ezgif.com-optimize__10_.gif">
+		</div>
+		<div class="item">
+			<a href="https://wonderurl.com" target="_blank">Wonderurl</a>
+			<h3>Wrap a unique digital gift and send it to your friends or customers</h3>
+			<img src="https://cdn.dribbble.com/users/886569/screenshots/5459050/ezgif.com-optimize__10_.gif">
+		</div>
+		<div class="item">
+			<a href="https://scaleboot.com" target="_blank">Scaleboot</a>
+			<h3>Service to validate minimum viable products &amp; launch them successfully</h3>
+			<img src="https://cdn.dribbble.com/users/886569/screenshots/5459050/ezgif.com-optimize__10_.gif">
+		</div>
+		<div class="item">
+			<a href="https://studiosnoon.com/figmaslides" target="_blank">Figma to Slides</a>
+			<h3>Convert Figma frames into a Google Slides presentation</h3>
+			<img src="https://cdn.dribbble.com/users/886569/screenshots/5459050/ezgif.com-optimize__10_.gif">
+		</div>
+	</div>
+	<div class="section">
+		<div class="intro">EXPERIMENTS <img src="assets/joy.png"></div>
+		<hr>
+		<div class="item">
+			<a href="https://exposure.cards/randomhunt" target="_blank">Random Hunt</a>
+			<h3>Discover random products from Product Hunt, on the press of a button</h3>
+			<img src="https://cdn.dribbble.com/users/886569/screenshots/5459050/ezgif.com-optimize__10_.gif">
+		</div>
+		<div class="item">
+			<a href="https://exposure.cards" target="_blank">Google Sheets CMS</a>
+			<h3>Using Google Sheets as a database or a CMS for any purpose</h3>
+			<img src="https://cdn.dribbble.com/users/886569/screenshots/5459050/ezgif.com-optimize__10_.gif">
+		</div>
+		<div class="item">
+			<a href="https://wonderurl.com" target="_blank">Simple Todo</a>
+			<h3>Minimal task manager built in ReactJs</h3>
+			<img src="https://cdn.dribbble.com/users/886569/screenshots/5459050/ezgif.com-optimize__10_.gif">
+		</div>
+	</div>
+	<div id="contact">
+		<img id="wave" src="assets/wave.png"> If you&#39;d like to talk, just send me an email at <a href="mailto:hi@alyssax.com" target="_blank">hi@alyssax.com</a>
+		<div id="status">
+			<img src="assets/status.svg"><p>Available for opportunities</p>
+		</div>
+	</div>
+	<div id="footer">
+		<hr>
+		<p>ALYSSA X &copy; <?php echo date("Y"); ?></p>
+		<img src="assets/canada.png">
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="main.js"></script>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	// Show & hide the images
+	$(".item a").hover(function(){
+	  $(this).parent().find("img").addClass("ishover");
+	  }, function() {
+	  $(this).parent().find("img").removeClass("ishover");
+	  });
+	
+	// Parallax effect
+	$(".item a").mousemove(function(e){
+	  // Get the Y offset from the first product
+	  var offset = $(this).offset().top-$(":first-child", $(".item")).offset().top;
+	  // Calculate the new position
+	  var movex = (e.pageX * -1.73 / 6);
+	  var movey = ((e.pageY-offset) * -1.73 / 6)+($(this).parent().find("img").height()/2)-35;
+	  // Apply the margins
+	  $(this).parent().find("img").css('margin-left', movex + 'px')/2;
+	  $(this).parent().find("img").css('margin-top', movey + 'px');
+	});
+});
